7c66bbab356b0d1b8582bab963121a46702d9410
Pedro Barros <pedroedson12@hotmail.com>
2018-11-09 14:37:44 -0300

    Initial commit

---
+++ test/negocio/ContaCorrenteTest.java
@@ -0,0 +1,16 @@
+package negocio;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class ContaCorrenteTest {
+    
+    public ContaCorrenteTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
---
+++ test/negocio/GerenciadoraClientesTest.java
@@ -0,0 +1,14 @@
+package negocio;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class GerenciadoraClientesTest {
+     
+
+    @Test
+    public void testPesquisaCliente() {
+    }
+    
+}
