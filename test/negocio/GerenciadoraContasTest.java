d9b2ac0a514aeefe5c7fcea25f4c70335d98673c
Pedro Barros <pedroedson12@hotmail.com>
2018-11-09 15:13:53 -0300

    Documentando e comentando os tests e metodos das classes

--- test/negocio/GerenciadoraContasTest.java
+++ test/negocio/GerenciadoraContasTest.java
@@ -10,10 +10,25 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Classe de teste criada para garantir o funcionamento das principais operações
+ * sobre contas, realizadas pela classe {@link GerenciadoraContas}.
+ *
+ * @author Pedro Barros
+ * @date 09/11/2018
+ */
 public class GerenciadoraContasTest {
 
     private GerenciadoraContas gerContas;
 
+    /**
+     * Teste básico da transferência de um valor da conta de um cliente para
+     * outro, estando ambos os clientes ativos e havendo saldo suficiente para
+     * tal transferência ocorrer com sucesso.
+     *
+     * @author Pedro Barros
+     * @date 09/11/2018
+     */
     @Test
     public void testTransfereValor() {
         /* ========== Montagem do cenário ========== */
@@ -30,8 +45,9 @@ public class GerenciadoraContasTest {
         gerContas = new GerenciadoraContas(contasDoBanco);
 
         /* ========== Execução ========== */
-        gerContas.transfereValor(1, 100, 2);
-        
+        boolean transferidoComSucesso = gerContas.transfereValor(1, 100, 2);
+
+        assertTrue(transferidoComSucesso);
         assertThat(conta01.getSaldo(), is(100.0));
         assertThat(conta02.getSaldo(), is(100.0));
     }
